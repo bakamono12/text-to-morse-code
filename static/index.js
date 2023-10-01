b87bc345d21db492319a11a6d8a9c057e677d94f
baka <abhishek>
2023-10-01 20:36:59 +0530

    Added some styling and some background for rain drop effects.

--- static/index.js
+++ static/index.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    $("#convert-button").click(function() {
+$(document).ready(function () {
+    $("#convert-button").click(function () {
         const userInput = $("#input-text").val().trim().toUpperCase();
         const morseOutput = $("#morse-output");
 
@@ -11,13 +11,53 @@ $(document).ready(function() {
         $.ajax({
             url: `/convert?text=${encodeURIComponent(userInput)}`,
             method: "GET",
-            success: function(data) {
+            success: function (data) {
                 morseOutput.val(data);
             },
-            error: function(xhr, status, error) {
+            error: function (xhr, status, error) {
                 console.error("An error occurred:", error);
                 morseOutput.val("An error occurred.");
             }
         });
     });
 });
+const canvas = document.getElementById('Matrix');
+const context = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const katakana = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン';
+const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const nums = '0123456789';
+
+const alphabet = katakana + latin + nums;
+
+const fontSize = 16;
+const columns = canvas.width / fontSize;
+
+const rainDrops = [];
+
+for (let x = 0; x < columns; x++) {
+    rainDrops[x] = 1;
+}
+
+const draw = () => {
+    context.fillStyle = 'rgba(0, 0, 0, 0.05)';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    context.fillStyle = '#0F0';
+    context.font = fontSize + 'px monospace';
+
+    for (let i = 0; i < rainDrops.length; i++) {
+        const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+        context.fillText(text, i * fontSize, rainDrops[i] * fontSize);
+
+        if (rainDrops[i] * fontSize > canvas.height && Math.random() > 0.975) {
+            rainDrops[i] = 0;
+        }
+        rainDrops[i]++;
+    }
+};
+
+setInterval(draw, 30);
